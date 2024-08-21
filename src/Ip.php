b54031ddf340b757394ed53202dbe51ccef87efe
linhw123 <136999667@qq.com>
2024-08-21 18:23:48 +0800

    test

---
+++ src/Ip.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Linhw\Ip;
+
+use GuzzleHttp\Client;
+
+class Ip
+{
+    public function getCountry($ip)
+    {
+        $client = new Client();
+        $response = $client->get("https://get.geojs.io/v1/ip/geo/{$ip}.json");
+        if ($response->getStatusCode() == 200) {
+            return json_decode($response->getBody());
+        }
+        return false;
+    }
+}
